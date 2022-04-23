1b2489d463fba856f7526cf29ff4b4016e068fde
Bora <94193278+boraaoz@users.noreply.github.com>
2022-04-23 20:15:36 +0300

    Add files via upload
    background and card component added

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Card.css'
+
+
+function Card() {
+  return (
+    <div className="overlay">
+     <div className="yarrgam">
+         <div className='headers'>Mint and transfer NFTs on Mina Protocol!</div>
+         <div className='card2'>NFT pic</div>
+         <button className='mint_button'>Beni sik!</button>
+    </div>
+  </div>
+  )
+}
+
+export default Card
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import './styles.css'
+import App from './App'
+import Card from './Card'
+
+ReactDOM.render(
+  <>
+
+    <App />
+    <Card />
+  
+  </>,
+  document.getElementById('root')
+)
